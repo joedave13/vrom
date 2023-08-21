063edd6b696725a5443aedbad27e8c0115d7b3fd
Joshua Davian <joshuadavian@gmail.com>
2023-08-21 12:45:38 +0700

    car model & migration

---
+++ app/Models/Car.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Car extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'slug', 'type_id', 'brand_id', 'price', 'rating'];
+}

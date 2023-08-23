7c3321f092717f0726428401a6cefa8469f38e6f
Joshua Davian <joshuadavian@gmail.com>
2023-08-23 12:41:03 +0700

    user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()->create([
+            'name' => 'Admin',
+            'email' => 'admin@vrom.test',
+            'password' => bcrypt('admin'),
+            'role' => 'ADMIN'
+        ]);
+    }
+}
